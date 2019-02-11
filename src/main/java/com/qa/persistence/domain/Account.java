53f6ca9e0f82b2ca5fe86481111260cd69f0a296
smurfjames101 <smurfjames@gmail.com>
2019-02-11 15:52:08 +0000

    added account constants

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -1,7 +1,53 @@
 package com.qa.persistence.domain;
 
 public class Account {
-	
-	//A comment
+
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private int accountNumber;
+
+	public Account()
+	{
+		
+	}
+	public Account(Long id, String firstName, String lastName, int accountNumber) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.accountNumber = accountNumber;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
 
 }
--- src/test/java/com/qa/MapTests/AccountServiceTest.java
+++ src/test/java/com/qa/MapTests/AccountServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.qa.persistence.domain.Account;
@@ -11,65 +12,67 @@ import com.qa.persistence.repository.AccountMapRepository;
 
 public class AccountServiceTest {
 
+	AccountMapRepository repo;
+
 	@Before
 	public void setup() {
-	
+		repo = new AccountMapRepository();
 	}
-	
+
 	@Test
 	public void addAccountTest() {
-		fail("TODO");	
+		assertEquals("Account Added, size:", repo.createAccount("oranges"));
 	}
 	
-	@Test
-	public void add2AccountsTest() {
-		fail("TODO");	
-	}
+//	@Ignore
+//	@Test
+//	public void add2AccountsTest() {
+//		fail("TODO");
+//	}
 
 	@Test
 	public void removeAccountTest() {
-		fail("TODO");	
+		assertEquals("Account Deleted",repo.deleteAccount((long) 1));
 	}
-	
-	@Test
-	public void remove2AccountsTest() {
-		fail("TODO");	
-	}
-	
-	@Test
-	public void remove2AccountTestAnd1ThatDoesntExist() {
-		fail("TODO");	
-	}
-	
-	@Test
-	public void jsonStringToAccountConversionTest() {
-		// testing JSONUtil
-		fail("TODO");	
-	}
-
-
-	@Test
-	public void accountConversionToJSONTest() {
-		//testing JSONUtil
-		fail("TODO");	
-	}
-
-	@Test
-	public void getCountForFirstNamesInAccountWhenZeroOccurances() {
-		//For a later piece of functionality
-		fail("TODO");	
-	}
-	
-	@Test
-	public void getCountForFirstNamesInAccountWhenOne() {
-		//For a later piece of functionality
-		fail("TODO");	
-	}
-
-	@Test
-	public void getCountForFirstNamesInAccountWhenTwo() {
-		//For a later piece of functionality
-		fail("TODO");	
-	}
+//	@Ignore
+//	@Test
+//	public void remove2AccountsTest() {
+//		fail("TODO");
+//	}
+//	@Ignore
+//	@Test
+//	public void remove2AccountTestAnd1ThatDoesntExist() {
+//		fail("TODO");
+//	}
+//	@Ignore
+//	@Test
+//	public void jsonStringToAccountConversionTest() {
+//		// testing JSONUtil
+//		fail("TODO");
+//	}
+//	@Ignore
+//	@Test
+//	public void accountConversionToJSONTest() {
+//		// testing JSONUtil
+//		fail("TODO");
+//	}
+//	@Ignore
+//	@Test
+//	public void getCountForFirstNamesInAccountWhenZeroOccurances() {
+//		// For a later piece of functionality
+//		fail("TODO");
+//	}
+//	@Ignore
+//	@Test
+//	public void getCountForFirstNamesInAccountWhenOne() {
+//		// For a later piece of functionality
+//		fail("TODO");
+//	}
+//	@Ignore
+//	@Test
+//	public void getCountForFirstNamesInAccountWhenTwo() {
+//		// For a later piece of functionality
+//		fail("TODO");
+//	}
 
 }
