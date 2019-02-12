83dc92b2c930bee2ebd6b557801dfecb43324f9f
smurfjames101 <smurfjames@gmail.com>
2019-02-12 15:05:59 +0000

    No idea this is qquick

--- src/main/java/com/qa/persistence/repository/AccountRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountRepository.java
@@ -1,10 +1,12 @@
 package com.qa.persistence.repository;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public interface AccountRepository {
 
 	String getAllAccounts();
 	String createAccount(String account);
 	String deleteAccount(Long id, int account);
 	String updateAccount(Long id, String account);
-
+	int cycleAccounts(String accountName) throws JsonProcessingException;
 }
--- src/test/java/com/qa/MapTests/AccountServiceTest.java
+++ src/test/java/com/qa/MapTests/AccountServiceTest.java
@@ -6,14 +6,30 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.qa.persistence.domain.App;
 import com.qa.persistence.repository.AccountMapRepository;
 
 public class AccountServiceTest {
 	AccountMapRepository repo = new AccountMapRepository();
 	@Before
 	public void setup() {
+		
 	}
-	
+	@Ignore
+	@Test
+	public void testCycleAccounts()
+	{
+		repo.createAccount("{\"firstName\":\"Bobby\",\"lastName\":\"Bobson\",\"accountNumber\":666}");
+		repo.createAccount("{\"firstName\":\"Bobby\",\"lastName\":\"Bobson\",\"accountNumber\":666}");
+		repo.createAccount("{\"firstName\":\"Bobby\",\"lastName\":\"Bobson\",\"accountNumber\":666}");
+		assertEquals(3,repo.cycleAccounts("Bobby"));
+	}
+	@Test
+	public void testLog()
+	{
+		assertEquals("success",App.log());
+	}
 	@Test
 	public void addAccountTest() {
 		assertEquals("Success",repo.createAccount("{\"firstName\":\"Bobby\",\"lastName\":\"Bobson\",\"accountNumber\":666}"));
@@ -27,13 +43,13 @@ public class AccountServiceTest {
 	
 	@Test
 	public void removeAccountTest() {
-		assertEquals("Account has been removed.",repo.deleteAccount((long) 1,1));
+		assertEquals("Account has been removed.",repo.deleteAccount((long) 0,666));
 			
 	}
 	
 	@Test
 	public void testGetAllAccounts() {
-		assertEquals("{\"0\":{\"firstName\":\"Bobby\",\"lastName\":\"Bobson\",\"accountNumber\":666},\"1\":{\"firstName\":\"Johnny\",\"lastName\":\"Bravo\",\"accountNumber\":42}}",repo.getAllAccounts());
+		assertEquals("Success",repo.getAllAccounts());
 	}
 	@Test
 	public void remove2AccountsTest() {	
